5a730cf28c790f1dd26ce5027e2c9f548a45e33f
kaivongcom <hello@kaivong.dev>
2025-09-23 11:32:58 +0100

    Update javascript enhancments

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,15 +1,10 @@
 var body_element = ' ',
-    initial_look = 'defaults-00-look'
-    looks1 = 'green-black-01-look',
-    looks2 = 'yellow-black-02-look',
-    looks6 = 'clay-06-look'
-    looks5 = 'redd-05-look'
-    looks4 = 'slime-04-look'
-    snow_on_blprnt = 'snowy-bluep-03-look'
+    initial_look = 'defaults-00-look', looks1 = 'green-black-01-look',
+    looks2 = 'yellow-black-02-look', looks6 = 'clay-06-look',
+    looks5 = 'redd-05-look', looks4 = 'slime-04-look', snow_on_blprnt = 'snowy-bluep-03-look'
     styled_looks = [initial_look, looks1, looks2, snow_on_blprnt, looks4, looks5, looks6],
     looksListName ='look-linker-list',
-    javascript_info = 'off-javascript-enable-message',
-    bodyHTML_classList = '';
+    javascript_info = 'off-javascript-enable-message', bodyHTML_classList = '';
 
 function getFirstBodyElement(){
   return document.getElementsByTagName('body')[0];
