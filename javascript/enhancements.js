cc88e90c2065a816dbea78f2ebe098500462161a
kaivongcom <hello@kaivong.dev>
2025-09-24 12:39:26 +0100

    Update theme picker to select

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,8 +1,8 @@
 var body_element = ' ',
-    initial_look = 'defaults-00-look', looks1 = 'green-black-01-look',
-    looks2 = 'yellow-black-02-look', looks6 = 'clay-06-look',
-    looks5 = 'redd-05-look', looks4 = 'slime-04-look', snow_on_blprnt = 'snowy-bluep-03-look'
-    styled_looks = [initial_look, looks1, looks2, snow_on_blprnt, looks4, looks5, looks6],
+    initial_look = 'defaults-02-look', looks1 = 'green-black-03-look',
+    looks2 = 'yellow-black-04-look', looks6 = 'clay-07-look',
+    looks5 = 'redd-06-look', looks4 = 'slime-05-look', snow_on_blprnt = 'snowy-bluep-01-look'
+    styled_looks = [snow_on_blprnt, initial_look, looks1, looks2, looks4, looks5, looks6],
     looksListName ='look-linker-list',
     javascript_info = 'off-javascript-enable-message', bodyHTML_classList = '';
 
@@ -23,8 +23,14 @@ function hideJavascriptProgressiveEnhancementMessage(){
 }
 
 function loadThisLooks(specific_look_integer){
+  
+  
   for(look_integer=0; look_integer <= 6; look_integer+=1 ){
     bodyHTML_classList.remove(styled_looks[look_integer]);  
   }
+  specific_look_integer = document.getElementById("look-linker-list").selectedIndex;
+  console.log('oh..' + specific_look_integer);
   bodyHTML_classList.add(styled_looks[specific_look_integer]);
 }
+
+
