3507f92013aa861994750c4af0042788f3009c4f
kaivongcom <hello@kaivong.dev>
2025-09-29 21:43:22 +0100

    edit javascript

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -1,13 +1,28 @@
-var body_element = ' ', style_index=false, params_arr=[], url_arr=[], links_counter=false,
-    current_link_edit=false, new_page_link=false,new_link_edit=false,
-    initial_look = 'defaults-02-look', looks1 = 'green-black-03-look', params_names = ['loadThisLooks'],
-    looks2 = 'yellow-black-04-look', looks6 = 'clay-07-look',
-    looks5 = 'redd-06-look', looks4 = 'slime-05-look', snow_on_blprnt = 'snowy-bluep-01-look',
-    looks7 = 'grapevine-08-look', l_9= 'leckiday-tesla-09-look', l10 = 'purpl-10-look',
-    styled_looks_arr = [snow_on_blprnt, initial_look, looks1, looks2, looks4, looks5, looks6, looks7, l_9,l10],
-    looksListName ='look-linker-list', javascript_info = 'off-javascript-enable-message', bodyHTML_classList = '',      
+var body_element = ' ', 
+    style_index = false, 
+    params_arr = [false, false], 
+    url_arr = [], 
+    links_counter = false,
+    current_link_edit = false, 
+    new_page_link = false,
+    new_link_edit = false, 
+    looks01 = 'snowy-bluep-01-look',
+    looks02 = 'defaults-02-look',
+    looks03 = 'green-black-03-look',     
+    looks04 = 'yellow-black-04-look', 
+    looks05 = 'slime-05-look',
+    looks06 = 'redd-06-look', 
+    looks07 = 'clay-07-look', 
+    looks08 = 'grapevine-08-look', 
+    looks09 = 'leckiday-tesla-09-look', 
+    looks10 = 'purpl-10-look',
+    styled_looks_arr = [looks01,looks02,looks03,looks04,looks05,looks06,looks07,looks08,looks09,looks10],
+    looksListName ='look-linker-list', 
+    javascript_info = 'off-javascript-enable-message', 
+    bodyHTML_classList = '',      
     links_domain = ['news-kaivong', 'x-kaivong',  'twitter-kaivong', 'homepage-back'],
-    links_domain_count = links_domain.length;
+    links_domain_count = links_domain.length,
+    params_names = ['loadThisLooks'];
 
 function getFirstBodyElement(){
   return document.getElementsByTagName('body')[0];
@@ -29,14 +44,12 @@ function uriStyledLooks(){
       document.getElementById(looksListName).selectedIndex = style_index;
     }
   } catch (error) {  }
-  
   return params_arr[1];
 }
 
 function onLoadDocumentWindowPage(){
   body_element = getFirstBodyElement();
   bodyHTML_classList = body_element.classList;
-  loadThisLooks(1);
   hideJavascriptProgressiveEnhancementMessage();
   uriStyledLooks();
   updateLinkColoursParams();
@@ -52,18 +65,21 @@ function hideJavascriptProgressiveEnhancementMessage(){
 function loadListLooks(){
   specific_look_integer = document.getElementById(looksListName).selectedIndex;
   loadThisLooks(specific_look_integer);
-  updateLinkColoursParams(specific_look_integer);
+  updateLinkColoursParams(String(specific_look_integer));
+  return false;
 }
 
 function loadThisLooks(specific_look_integer){
-  for(look_integer=0; look_integer <= 10; look_integer+=1 ){
+  for(look_integer=0; look_integer <= 10;look_integer){
     bodyHTML_classList.remove(styled_looks_arr[look_integer]);
+    look_integer += 1;
   }
   bodyHTML_classList.add(styled_looks_arr[specific_look_integer]);
+  return false;  
 }
 
 function updateLinkColoursParams(specific_look_integer=false){
-  for(links_counter=0; links_counter <= links_domain_count; links_counter+=1){
+  for(links_counter=0; links_counter <= links_domain_count; links_counter){
     current_link_edit = links_domain[links_counter];
     current_link_node = document.getElementById('link-'+current_link_edit);
     if (!specific_look_integer){
@@ -74,5 +90,6 @@ function updateLinkColoursParams(specific_look_integer=false){
       new_link_edit = new_page_link + '?' + params_names[0] + '=' + specific_look_integer;
       current_link_node.href = new_link_edit; 
     }
+    links_counter+=1;
   }  
 }
