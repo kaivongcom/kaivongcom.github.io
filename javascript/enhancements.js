2bdec143325cca690aa6e3422d20b4b07157d713
kaivongcom <hello@kaivong.dev>
2025-09-30 21:18:55 +0100

    edit styles

--- javascript/enhancements.js
+++ javascript/enhancements.js
@@ -21,7 +21,9 @@ var specific_look_integer = false,
     looks11 = 'blue1-11-look',
     looks12 = 'blue2-12-look',
     looks13 = 'blue3-13-look',
-    styled_looks_arr = [looks01,looks02,looks03,looks04,looks05,looks06,looks07,looks08,looks09,looks10,looks11,looks12,looks13],
+    looks14 = 'blue4-14-look',
+    looks_available = 14,
+    styled_looks_arr = [looks01,looks02,looks03, looks04,looks05,looks06,looks07,looks08,looks09,looks10,looks11,looks12,looks13,looks14],
     looksListName ='look-linker-list', 
     javascript_info = 'off-javascript-enable-message', 
     bodyHTML_classList = '',      
@@ -75,7 +77,7 @@ function loadListLooks(){
 }
 
 function loadThisLooks(specific_look_integer){
-  for(look_integer=0; look_integer <= 13;look_integer){
+  for(look_integer=0; look_integer <= looks_available;look_integer){
     bodyHTML_classList.remove(styled_looks_arr[look_integer]);
     look_integer += 1;
   }
