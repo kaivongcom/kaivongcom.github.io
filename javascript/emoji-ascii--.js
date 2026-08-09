b3b6bf346268de1ad639770ea7d8f8d6d08c5caa
kaivongcom <hello@kaivong.dev>
2026-08-09 10:16:08 +0100

    adds enhancement for completers

--- javascript/emoji-ascii--.js
+++ javascript/emoji-ascii--.js
@@ -1,12 +1,25 @@
-emoji_ascii = { 
-':D': {'emote': '😀'}, 
-'=D': {'emote': '😃'}, 
-'^D': {'emote': '😄'}, 
-'^#': {'emote': '😁'}, 
-'ii': { 'emote': '🥢🥡'}, 
-'lēk': { 'emote': '😇📚' }, 
-'--叻': { 'emote': '😇📚' }, 
-'kekeke': {'emote': 'IMG_KEEE.png', 
-'info':'from video-game nintendo, Game Freak, published by The Pokémon Company, Pokémon LeafGreen: Channeler trainers'}
+function emoji_ascii_keys(){
+  return emoji_ascii_keys_found;
+}
+function emoji_ascii_values(){
+  return emoji_ascii_values_found;
 }
 
+emoji_ascii_keys_found = [':D', '=D', '^D', '^#', 'ii', 'lēk', '🍪', '--叻', 'kekeke'];
+emoji_ascii_values_found = [{'emote': '😀'}, {'emote': '😃'}, {'emote': '😄'}, {'emote': '😁'}, 
+{'emote': '🥢🥡'}, {'emote': '😇📚'}, {'emote': '🍪'}, {'emote': '😇📚'}, 
+{'emote': 'IMG_KEEE.png', 'info': 'from video-game nintendo, Game Freak, published by The Pokémon Company, Pokémon LeafGreen: Channeler trainers'}];
+
+emoji_ascii = { 
+//  ':t': { 'emote': '🐌', 'info': 'film' }, 
+  ':D': {'emote': '😀'}, 
+  '=D': {'emote': '😃'}, 
+  '^D': {'emote': '😄'}, 
+  '^#': {'emote': '😁'}, 
+  'ii': { 'emote': '🥢🥡'}, 
+  'lēk': { 'emote': '😇📚' }, 
+  '🍪': { 'emote': '🍦<span class=cookies><span>🍪🍪🍪🍪🍪<span></br><span>🍪🍪🍪🍪🍪<span></br><span>🍪🍪🍪🍪🍪<span></br><span>🍪🍪🍪🍪🍪<span></br><span>🍪🍪🍪🍪🍪<span></span>' }, 
+  '--叻': { 'emote': '😇📚' }, 
+  'kekeke': {'emote': 'IMG_KEEE.png', 
+             'info':'from video-game nintendo, Game Freak, published by The Pokémon Company, Pokémon LeafGreen: Channeler trainers'}
+}
